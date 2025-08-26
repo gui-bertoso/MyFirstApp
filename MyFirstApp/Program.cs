0ff003888d117f7232da6365d5372353a7596a1f
gbertoso <guilhermedasilvabertoso@gmail.com>
2025-08-26 14:08:43 -0300

    Initial commit

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MyFirstApp;
+
+public class MyApp
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i + " é par");
+            }
+            else
+            {
+                Console.WriteLine(i + " is impar");
+            }
+        }
+    }
+}
